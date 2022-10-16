59f80027000335e062c9a22fc08360b8db4197db
Dmitry <roddg86@gmail.com>
2022-10-16 10:17:27 +0900

    add Example 009 Intro Array project

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Функция нахождения максимального числа из трех*/
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = { 41, 32, 13, 45, 53, 62, 17, 48, 19 };
+
+int max = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
+
+Console.WriteLine(max);
